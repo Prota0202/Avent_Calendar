0b23a71e2c1798e9a0ab2989b29799e2a68a6bd3
Prota <Abdelbadi5@protonmail.com>
2024-12-23 18:14:15 +0100

    Exo 1

---
+++ Exo1/exo1.py
@@ -0,0 +1,22 @@
+file_path = "C:\\Users\\AbdelBadi\\Desktop\\algo\\1.txt"
+data = open(file_path, "r")
+
+gauche = []
+droite = []
+
+for elem in data :
+    l = elem[:-1].split(" ")
+    gauche.append(int(l[0]))
+    droite.append(int(l[-1]))
+
+gauche.sort()
+droite.sort()
+
+total = 0
+
+for i in range(len(gauche)) :
+    total = total + abs(gauche[i] - droite[i])
+
+print(total)
+
+#############################################################
